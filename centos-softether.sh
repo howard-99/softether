8a0ded6c53b1be492f49896300f09dce98821328
howard-99 <115505209+howard-99@users.noreply.github.com>
2023-02-18 15:44:02 +0330

    Update centos-softether.sh

--- centos-softether.sh
+++ centos-softether.sh
@@ -16,6 +16,32 @@ chmod 755 /etc/init.d/vpnserver
 /etc/init.d/vpnserver start
 chkconfig --add vpnserver 
 
+#Firewalld
+sudo yum install firewalld -y
+sudo systemctl start firewalld
+sudo systemctl enable firewalld
+
+sudo firewall-cmd --zone=public --add-port=22/tcp
+sudo firewall-cmd --zone=public --add-port=443/tcp
+sudo firewall-cmd --zone=public --add-port=80/tcp
+sudo firewall-cmd --zone=public --add-port=992/tcp
+sudo firewall-cmd --zone=public --add-port=1194/tcp
+sudo firewall-cmd --zone=public --add-port=5555/tcp
+sudo firewall-cmd --zone=public --add-port=4500/tcp
+sudo firewall-cmd --zone=public --add-port=1701/tcp
+sudo firewall-cmd --zone=public --add-port=500/tcp
+sudo firewall-cmd --zone=public --add-port=50740/tcp
+sudo firewall-cmd --zone=public --add-port=8443/tcp
+sudo firewall-cmd --zone=public --add-port=9699/tcp
+sudo firewall-cmd --zone=public --add-port=2083/tcp
+sudo firewall-cmd --zone=public --add-port=8080/tcp
+sudo firewall-cmd --zone=public --add-port=443/udp
+sudo firewall-cmd --zone=public --add-port=500/udp
+sudo firewall-cmd --zone=public --add-port=4500/udp
+sudo firewall-cmd --zone=public --add-port=1194/udp
+
+
+
 yum -y update
 
 clear
